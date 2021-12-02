1ddd7d5e0888b98bc38c76c970c542d124da3b88
youth <youth1212@163.com>
2021-12-02 12:23:13 +0800

    test

---
+++ exec.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+exec  < file_name
+while [ $? -eq 0 ]
+do
+print "$FILE_NAME"
+read FILE_NAME
+done
---
+++ test.sh
@@ -0,0 +1,9 @@
+this is 1st 
+this is 2nd
+this is 3st
+this is 4st
+this is 5st
+this is 6st
+7 
+ 8
+ 9
